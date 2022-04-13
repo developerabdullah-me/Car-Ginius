e6926ce9192b9bf0a09923a23b7f71993af7bc73
developerabdullah-me <abdullahtalha9999@gmail.com>
2022-04-13 23:48:17 +0600

    fake data

---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Servieces from '../Services/Servieces';
+
+const Home = () => {
+    return (
+        <div>
+          <Servieces></Servieces>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/pages/Home/Services/Servieces.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react';
+
+const Servieces = () => {
+  const [services,setServices]=useState([])
+  useEffect(() =>{
+      
+  },[])
+    return (
+        <div>
+          
+        </div>
+    );
+};
+
+export default Servieces;
