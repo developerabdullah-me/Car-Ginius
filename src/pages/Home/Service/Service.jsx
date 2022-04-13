fb8ee9537e407a76e3240d14315b05ce2ef3627e
developerabdullah-me <abdullahtalha9999@gmail.com>
2022-04-14 01:16:24 +0600

    responcive

--- src/pages/Home/Service/Service.jsx
+++ src/pages/Home/Service/Service.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import './Service.css'
 const Service = (props) => {
     const {name,img,description,price}=props.service;
     return (
-        <div>
+        <div className='service-Content'>
             <img src={img} alt="" />
             <h1>{name}</h1>
-            <p>{price}</p>
+            <p>Price:{price}</p>
         </div>
     );
 };
