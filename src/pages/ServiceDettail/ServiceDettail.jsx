39fe2af45c7206a2bba8ee902060c323f179ae8f
developerabdullah-me <abdullahtalha9999@gmail.com>
2022-04-14 13:35:31 +0600

    useparams

--- src/pages/ServiceDettail/ServiceDettail.jsx
+++ src/pages/ServiceDettail/ServiceDettail.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const ServiceDettail = () => {
+    const {serviceId}=useParams()
     return (
         <div>
-            <h1>welcome</h1>
+            <h1>welcome{serviceId}</h1>
         </div>
     );
 };
