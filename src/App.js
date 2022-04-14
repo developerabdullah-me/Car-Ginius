000e0ed3df8e7b638fd7bb07cb5eb4bde99003cf
developerabdullah-me <abdullahtalha9999@gmail.com>
2022-04-14 14:58:08 +0600

    fore fore

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './pages/About/About';
 import Home from './pages/Home/Home/Home';
+import Login from './pages/Login/Login';
+import Notfownd from './pages/Notfownd/Notfownd';
 import ServiceDettail from './pages/ServiceDettail/ServiceDettail';
 import Footer from './pages/sheare/Footer/Footer';
 import Header from './pages/sheare/Header/Header';
@@ -16,6 +18,8 @@ function App() {
          <Route path='/home' element={<Home></Home>}></Route>
          <Route path='/service/:serviceId' element={<ServiceDettail></ServiceDettail>}></Route>
          <Route path='/about' element={<About></About>}></Route>
+         <Route path='/login' element={<Login></Login>}> </Route>
+         <Route path='*' element={<Notfownd></Notfownd>}></Route>
        </Routes>
        <Footer></Footer>
     </div>
---
+++ src/pages/Notfownd/Notfownd.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Notfownd = () => {
+    return (
+        <div>
+            <h1 className="text-red">404</h1>
+        </div>
+    );
+};
+
+export default Notfownd;
