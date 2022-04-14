b0515b216f6af0b5fdd356b6a517166ab952a699
developerabdullah-me <abdullahtalha9999@gmail.com>
2022-04-15 00:20:02 +0600

    plese regiter

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,49 +1,67 @@
-import React, { useRef } from 'react';
-import { Button, Form } from 'react-bootstrap';
-import { useNavigate } from 'react-router-dom';
+import React, { useRef } from "react";
+import { Button, Form } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
-    const emailRef=useRef('')
-    const passwordRef=useRef('')
-    const naviGate=useNavigate()
-    const handelSubmit=event=>{
-        event.preventDefault();
-        const email =emailRef.current.value;
-        const password =passwordRef.current.value;
-        console.log(email,password);
-    }
-    const naviGateRegister=event=>{
-        naviGate('/register')
-    }
-    return (
-        <div>
-            <h1>login</h1>
-            <div className="mx-auto w-50">
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
+  const naviGate = useNavigate();
+  const handelSubmit = (event) => {
+    event.preventDefault();
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+    console.log(email, password);
+  };
+  const naviGateRegister = (event) => {
+    naviGate("/register");
+  };
+  return (
+    <div>
+      <h1>login</h1>
+      <div className="mx-auto w-50">
+        <Form onSubmit={handelSubmit}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              ref={emailRef}
+              type="email"
+              placeholder="Enter email"
+              required
+            />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
 
-            <Form onSubmit={handelSubmit}>
-  <Form.Group className="mb-3" controlId="formBasicEmail">
-    <Form.Label>Email address</Form.Label>
-    <Form.Control ref={emailRef} type="email" placeholder="Enter email" required/>
-    <Form.Text className="text-muted">
-      We'll never share your email with anyone else.
-    </Form.Text>
-  </Form.Group>
-
-  <Form.Group className="mb-3" controlId="formBasicPassword">
-    <Form.Label>Password</Form.Label>
-    <Form.Control ref={passwordRef} type="password" placeholder="Password" required/>
-  </Form.Group>
-  <Form.Group className="mb-3" controlId="formBasicCheckbox">
-    <Form.Check type="checkbox" label="Check me out" />
-  </Form.Group>
-  <Button variant="primary" type="submit">
-    Submit
-  </Button>
-</Form>
-<p>New Car Genius?<span className="text-danger" onclick={naviGateRegister}>please register</span></p>
-            </div>
-        </div>
-    );
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              ref={passwordRef}
+              type="password"
+              placeholder="Password"
+              required
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+        <p>
+          New Car Genius?
+          <Link
+            to="/register  "
+            className="text-danger"
+            onClick={naviGateRegister}
+          >
+            please register
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/pages/Login/SignUp/SingUp.jsx
+++ src/pages/Login/SignUp/SingUp.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
-
+import { Link, useNavigate } from 'react-router-dom';
+import './SignUp.css'
 const SingUp = () => {
+    const naviGate=useNavigate()
+    const naviGatelogin=event=>{
+        naviGate('/register')
+    }
     return (
-        <div>
+        <div className="from-container">
             <h1>please register</h1>
+            <div>
+                <from>
+                <input type="name" name="name" placeholder="type your name" required/><br />
+                <input type="email" name="email" placeholder="type your email" required/><br />
+                <input type="password" name="password" placeholder="type your password" required/><br />
+                <input type="password" name="name" placeholder="type your confirm password" required/><br />
+                <input type="number" name="number" placeholder="type your number" required/><br />
+                <input type="submit" value="submit" />
+                </from>
+                <p>
+         all ready register
+          <Link
+            to="/login "
+            className="text-danger"
+            onClick={naviGatelogin}
+          >
+            please login
+          </Link>
+        </p>
+            </div>
         </div>
     );
 };
