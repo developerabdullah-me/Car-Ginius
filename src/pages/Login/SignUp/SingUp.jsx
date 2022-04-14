750391eb45d927d8a8a25d74d5e6a4cb8ea1e6cb
developerabdullah-me <abdullahtalha9999@gmail.com>
2022-04-15 01:19:27 +0600

    register  your email and password

--- src/pages/Login/SignUp/SingUp.jsx
+++ src/pages/Login/SignUp/SingUp.jsx
@@ -1,23 +1,42 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
 import './SignUp.css'
 const SingUp = () => {
     const naviGate=useNavigate()
     const naviGatelogin=event=>{
         naviGate('/register')
     }
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useCreateUserWithEmailAndPassword(auth);
+      if(user){
+          naviGate('/home')
+      }
+      const handleRegister = event =>{
+        event.preventDefault();
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+
+        createUserWithEmailAndPassword(email, password);
+    }
     return (
-        <div className="from-container">
+        <div className="from-container ">
             <h1>please register</h1>
             <div>
-                <from>
+                <form onSubmit={handleRegister}>
                 <input type="name" name="name" placeholder="type your name" required/><br />
                 <input type="email" name="email" placeholder="type your email" required/><br />
                 <input type="password" name="password" placeholder="type your password" required/><br />
                 <input type="password" name="name" placeholder="type your confirm password" required/><br />
                 <input type="number" name="number" placeholder="type your number" required/><br />
                 <input type="submit" value="submit" />
-                </from>
+                </form>
                 <p>
          all ready register
           <Link
