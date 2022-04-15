7ace993fd6fa89115f8c09671330832bd8a4412e
developerabdullah-me <abdullahtalha9999@gmail.com>
2022-04-15 17:11:32 +0600

    sign in google

--- src/pages/Login/Social/Social.jsx
+++ src/pages/Login/Social/Social.jsx
@@ -3,7 +3,22 @@ import googl from "../Social/Images/google-logo.png";
 import facebook from "../Social/Images/Facebook-logo.png";
 import github from "../Social/Images/github-logo.png";
 import twitter from "../Social/Images/twitter-logo.png";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import auth from "../../../firebase.init";
+import { useNavigate } from "react-router-dom";
 const Social = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const navigate=useNavigate()
+    if (user){
+        navigate('/home')
+    }
+let elementErrors ;
+    if (error) {
+        elementErrors= <div>
+            <p>Error: {error.message}</p>
+            </div>
+    }
+
   return (
     <div>
       <div className="d-flex align-items-center justify-content-center ">
@@ -13,11 +28,12 @@ const Social = () => {
 
         <div className=" bg-primary w-25" style={{ height: "1px" }}></div>
       </div>
+      {elementErrors}
 <div className=" ml-5">
 
     
 <div className="mx-auto  text-center " style={{}}>
-        <button className="btn-primary text-center p-2">
+        <button onClick={() =>signInWithGoogle()} className="btn-primary text-center p-2">
           {" "}
           <img style={{ width: "30px" }} src={googl} alt="" />
           Google sign in
