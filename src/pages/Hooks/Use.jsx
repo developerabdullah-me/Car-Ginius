89dc54eb4a67784fbcab25d42655617333d4c5f0
developerabdullah-me <abdullahtalha9999@gmail.com>
2022-04-24 17:12:42 +0600

    deletedata

--- src/pages/Hooks/Use.jsx
+++ src/pages/Hooks/Use.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import { useEffect, useState } from "react";
 
-const Use = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
-
-export default Use;
+const useService=()=>{
+    const [services,setServices]=useState([])
+    useEffect( ()=>{
+      fetch('http://localhost:5000/service')
+      .then(res => res.json())
+      .then(data => setServices(data));
+  }, []);
+  return  [services,setServices]
+}
+export default useService
--- src/pages/ManageService.jsx
+++ src/pages/ManageService.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
+import useService from './Hooks/Use';
 
 const ManageService = () => {
+    const [services,setServices]=useService();
+    const handelDelete=id=>{
+        const proceed=window.confirm('Are you sure you want to delete')
+        if(proceed){
+    const url=`http://localhost:5000/service/${id}`
+    fetch(url,{
+        method: 'DELETE',
+    })
+    .then(res=> res.json())
+    .then(data=>{
+        console.log(data);
+        const remaining=services.filter(service=>service._id !== id )
+        setServices(remaining)
+    })
+
+        }
+    }
     return (
-        <div>
+        <div className='w-50 mx-auto'>
             <h1>mage your Services</h1>
+            {
+                services.map(service => <div key={service._id}>
+                    <h5>
+                        {service.name}
+                        <button onClick={()=>handelDelete(service._id)}>X</button>
+                    </h5>
+                </div>)
+            }
         </div>
     );
 };
